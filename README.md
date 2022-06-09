08e56c28770fd2ec3f2f8450a6752f6155fba841
aleyire <97916623+aleyire@users.noreply.github.com>
2022-06-08 20:24:38 -0400

    desafio

---
+++ README.md
@@ -0,0 +1,19 @@
+## Mercado Web
+
+En este desafío se construyó un servidor con Express que utiliza el motor de plantillas Handlebars para desarrollar componentes y renderizar variables e iteradores en el template del sitio web.
+
+### Materia aplicada
+
+- Integrando jQuery y Bootstrap
+- Handlebars y los motores de plantilla
+- Parciales
+- Helpers
+- Devolviendo un sitio web de contenido dinámico
+
+### Pasos
+
+- npm init
+- npm i express
+- npm install --save jquery
+- npm install --save bootstrap
+- npm install --save express-handlebars
---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const app = express()
+const { engine } = require("express-handlebars")
+
+app.listen(3000, () => {
+  console.log("Server on")
+})
+
+app.use("/css", express.static(__dirname + "./node_modules/bootstrap/dist/css"))
+app.use("/js", express.static(__dirname + "./node_modules/bootstrap/dist/js"))
+app.use("/", express.static(__dirname + "./views/img"))
+
+app.set("view engine", "handlebars")
+
+app.engine(
+  "handlebars",
+  engine({
+    layoutsDir: __dirname + "/views",
+    partialsDir: __dirname + "/views/componentes/",
+  })
+)
+
+app.get("/", (req, res) => {
+  res.render("Menu", {
+    layout: "Menu",
+    productos: ["banana", "cebollas", "lechuga", "papas", "pimenton", "tomate"],
+  })
+})
+
+
+/*app.get("/", (req, res) => {
+  res.render("menu", { layout: "menu" })
+})*/
